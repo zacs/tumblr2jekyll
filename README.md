5add76c51d439c92969c06a7ba6d829d4c977359
Zac Schellhardt <zac@z4c.us>
2012-09-04 22:52:07 -0700

    Import now works fully, sans image support for images within posts. Added custom fields option for appending things to each post.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 tumblr2hyde
 ===========
 
-*Not currently working yet*. Script for pulling posts out of Tumblr and creating content for Hyde. 
+*Images within posts not working yet*. Script for pulling posts out of Tumblr and creating content for Hyde (or presumably Jekyll, but I'm not sure). 
 
 Usage
 -----
@@ -9,6 +9,8 @@ Usage
 1. Go to Tumblr and [request an API key](http://www.tumblr.com/oauth/register). 
 2. Paste the *OAuth Consumer Key* into the script as `API_KEY`.
 3. Enter your site's root Tumblr URL as `TUMBLR_ROOT`.
-4. In the terminal: `python tumblr2hyde.py`
+4. [Optional] Change the `TEXT_ONLY` setting to False if you want more than just your text posts.
+5. [Optional] Add tuples to the dictionary for customer fields. [More info](#custom_fields).
+6. In the terminal: `python tumblr2hyde.py` -- the directory structure for Hyde will be created wherever the script is run from.
 
 That should do it. No need really to set up a virtualenv, as there are no requirement modules outside of core Python libraries. 
--- tumblr2hyde.py
+++ tumblr2hyde.py
@@ -4,50 +4,82 @@ Author: Zac Schellhardt (zac@z4c.us)
 Website: http://github.com/zacs/tumblr2hyde
 """
 
-API_KEY = ""
-TUMBLR_ROOT = ""
+API_KEY = "YOUR_API_KEY"
+TUMBLR_ROOT = "YOUR_ROOT_DOMAIN"
+TEXT_ONLY = True
+CUSTOM_FIELDS = {"extends": "blog.j2", "default_block": "post", "listable": "true"}
 
 import urllib2
 import json
+import os
+import codecs
 
-def loadTumblr(knownPosts,
-			   tumblrUrl,
-			   apiKey,
-			   perPage=20,
-			   offset=0,
-			   textOnly=True):
-	""""docstring for loadTumblr"""
-	if textOnly==True:
-		url = "http://api.tumblr.com/v2/blog/%s/posts/text?api_key=%s&offset=%s&limit=%s" % (tumblrUrl, apiKey, offset, perPage)
-	else:
-		url = "http://api.tumblr.com/v2/blog/%s/posts?api_key=%s&offset=%s&limit=%s" % (tumblrUrl, apiKey, offset, perPage)
-	f = urllib2.urlopen(urllib2.Request(url))
-	data = json.load(f)
-	f.close()
-	if not data["response"]["posts"]:
-		return knownPosts
-	else:
-		if knownPosts==None:
-			return loadTumblr(data["response"]["posts"],
-							  tumblrUrl, apiKey, perPage, offset+20, textOnly)
-		else:
-			return loadTumblr(knownPosts+data["response"]["posts"],
-							  tumblrUrl, apiKey, perPage, offset+20, textOnly)
+def loadTumblr(knownPosts, tumblrUrl, apiKey, 
+               perPage=20, offset=0, textOnly=True):
+    """Return complete contents of a Tumblr blog (paging-aware)."""
+    if textOnly==True:
+        url = "http://api.tumblr.com/v2/blog/%s/posts/text?api_key=%s&offset=%s&limit=%s" % (tumblrUrl, apiKey, offset, perPage)
+    else:
+        url = "http://api.tumblr.com/v2/blog/%s/posts?api_key=%s&offset=%s&limit=%s" % (tumblrUrl, apiKey, offset, perPage)
+    f = urllib2.urlopen(urllib2.Request(url))
+    data = json.load(f)
+    f.close()
+    if not data["response"]["posts"]:
+        return knownPosts
+    else:
+        if knownPosts==None:
+            return loadTumblr(data["response"]["posts"],
+                              tumblrUrl, apiKey, perPage, offset+20, textOnly)
+        else:
+            return loadTumblr(knownPosts+data["response"]["posts"],
+                              tumblrUrl, apiKey, perPage, offset+20, textOnly)
+    pass
+
+def initializeDirs():
+    """docstring for initializeDirs"""
+    dirs = ["contents/blog/","media/images/"]
+    for dir in dirs:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 	pass
 
 def createSinglePost(tumblrData):
-	""""docstring for createSinglePost"""
-	pass
+    """Creates a single post's file and saves any images."""
+    fileLoc = "contents/blog/%s.html" % tumblrData["slug"]
+    fileLoc = fileLoc.encode("ASCII")
+    f = codecs.open(fileLoc, "w", "utf-8")
+    fileContents = "---\n"
+    fileContents += "title: %s\n" % tumblrData["title"]
+    fileContents += "description: >\n"
+    fileContents += "    %s\n" % tumblrData["title"]
+    fileContents += "created: !!timestamp '%s'\n" % tumblrData["date"]
+    fileContents += "tags:\n"
+    for tag in tumblrData["tags"]:
+        fileContents += "    - %s\n" % tag
+    for key, value in CUSTOM_FIELDS.iteritems():
+        fileContents += "%s: %s\n" % (key, value)
+    fileContents += "---\n\n"
+    fileContents += tumblrData["body"]
+    f.write(fileContents)
+    f.close()
+    mediaLoc = "media/images/"
+    downloadImages(tumblrData["body"], mediaLoc, tumblrData["slug"])
+    pass
 
-def downloadImages(body, directory):
-	""""docstring for downloadImages"""
-	pass
+def downloadImages(body, directory, slug):
+    """docstring for downloadImages"""
+    dir = directory + slug
+    #if not os.path.exists(dir):
+    #    os.makedirs(dir)
+    pass
 
 def main():
-	""""docstring for main"""
-	data = loadTumblr(None, TUMBLR_ROOT, API_KEY)
-	print data
-	pass
+    """docstring for main"""
+    data = loadTumblr(None, TUMBLR_ROOT, API_KEY, textOnly=TEXT_ONLY)
+    initializeDirs()
+    for post in data:
+        createSinglePost(post)
+    pass
 
 if __name__ == '__main__':
-	main()
+    main()
